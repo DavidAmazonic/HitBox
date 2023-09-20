742edbc37b195d45d8cd0cb94ada35e620f3e709
David Ricardo <larutmotors@hotmail.com>
2023-09-20 17:59:32 -0500

    jueguito pa paula

--- juegoAABB.py
+++ juegoAABB.py
@@ -1,105 +1,157 @@
 import turtle
 import time
 
+# Variables globales
+ins = 0
+hitbox = 20
 # Posponer
-timestop = 0.005
+aguanta = 0.005
+# Puntaje
+Puntaje = 0
 
 # Configuración ventana
 wn = turtle.Screen()
-wn.title("AABB")
-wn.bgcolor("white")
-wn.setup(width=600, height=600)
+wn.title("JUEGO AABB")
+wn.bgcolor("gray")
+wn.setup(width=800, height=500)
 wn.tracer(0)
 
+################################
+########## OBJETOS #############
 # Objeto movimiento
 
-person = turtle.Turtle()
-person.speed(2)
-person.shape("square")
-person.penup()
-person.goto(0, 0)
-person.direction = "stop"
+yo = turtle.Turtle()
+yo.speed(2)
+yo.shape("square")
+yo.penup()
+yo.goto(0, 0)
+yo.direction = "stop"
 
-# Objetos estatico
+# Circulo estatico
 
-ogject1 = turtle.Turtle()
-ogject1.speed(2)
-ogject1.shape("square")
-ogject1.color("black")
-ogject1.penup()
-ogject1.goto(100, 50)
-ogject1.direction = "stop"
+circulo = turtle.Turtle()
+circulo.speed(2)
+circulo.shape("circle")
+circulo.color("red")
+circulo.penup()
+circulo.goto(-200, 90)
+circulo.direction = "stop"
 
-# Objetos recojer 2
+# Circulo grande
 
-ogject2 = turtle.Turtle()
-ogject2.speed(2)
-ogject2.shape("square")
-ogject2.color("black")
-ogject2.penup()
-ogject2.goto(-50, -100)
-ogject2.direction = "stop"
-# ogject2.shapesize(0.5, 0.5)
+circulobig = turtle.Turtle()
+circulobig.speed(2)
+circulobig.shape("circle")
+circulobig.color("red")
+circulobig.penup()
+circulobig.goto(300, 100)
+circulobig.direction = "stop"
+circulobig.shapesize(1.3, 1.3)
+# Circulo pequeño
 
-# Variables
+circulosmall = turtle.Turtle()
+circulosmall.speed(2)
+circulosmall.shape("circle")
+circulosmall.color("red")
+circulosmall.penup()
+circulosmall.goto(150, -100)
+circulosmall.direction = "stop"
+circulosmall.shapesize(0.5, 0.5)
+# Trangulo estatico
 
-# funcion retroceder
+triangulo = turtle.Turtle()
+triangulo.speed(2)
+triangulo.shape("triangle")
+triangulo.color("brown")
+triangulo.penup()
+triangulo.goto(-200, -150)
+triangulo.direction = "stop"
+
+# Rectangulo estatico
+
+rectangulo = turtle.Turtle()
+rectangulo.speed(2)
+rectangulo.shape("square")
+rectangulo.color("green")
+rectangulo.penup()
+rectangulo.goto(200, -40)
+rectangulo.direction = "stop"
+
+# Cuadrado estatico
+
+cuadrado1 = turtle.Turtle()
+cuadrado1.speed(2)
+cuadrado1.shape("square")
+cuadrado1.color("green")
+cuadrado1.penup()
+cuadrado1.goto(-50, -100)
+cuadrado1.direction = "stop"
 
 
-def retroced():
-    if person.direction == "up":
-        y = person.ycor()
-        person.sety(y - 1)
-    if person.direction == "down":
-        y = person.ycor()
-        person.sety(y + 1)
-    if person.direction == "right":
-        x = person.xcor()
-        person.setx(x - 1)
-    if person.direction == "left":
-        x = person.xcor()
-        person.setx(x + 1)
+################################
+####### FUNCIONAMIENTO #########
 
-# Funciones moviment
+### FUNCION DE MOVIMIENTO ###
 
 
 def mov():
-    if person.direction == "up":
-        y = person.ycor()
-        person.sety(y + 1)
-    if person.direction == "down":
-        y = person.ycor()
-        person.sety(y - 1)
-    if person.direction == "right":
-        x = person.xcor()
-        person.setx(x + 1)
-    if person.direction == "left":
-        x = person.xcor()
-        person.setx(x - 1)
+    if yo.direction == "up":
+        y = yo.ycor()
+        yo.sety(y + 1)
+    if yo.direction == "down":
+        y = yo.ycor()
+        yo.sety(y - 1)
+    if yo.direction == "right":
+        x = yo.xcor()
+        yo.setx(x + 1)
+    if yo.direction == "left":
+        x = yo.xcor()
+        yo.setx(x - 1)
 
+
+### EVITAR QUE SE QUEDE QUIETO ###
+
+
+def retroced():
+    if yo.direction == "up":
+        yo.direction = "down"
+        for n in range(10):
+            mov()
+    elif yo.direction == "down":
+        yo.direction = "up"
+        for n in range(10):
+            mov()
+    elif yo.direction == "right":
+        yo.direction = "left"
+        for n in range(10):
+            mov()
+    elif yo.direction == "left":
+        yo.direction = "right"
+        for n in range(10):
+            mov()
 # Controles
 
 
 def up():
-    person.direction = "up"
+    yo.direction = "up"
 
 
 def down():
-    person.direction = "down"
+    yo.direction = "down"
 
 
 def left():
-    person.direction = "left"
+    yo.direction = "left"
 
 
 def right():
-    person.direction = "right"
+    yo.direction = "right"
 
 
 def stop():
     global ins
     if ins == 200:
-        person.direction = "stop"
+        yo.direction = "stop"
         ins = 0
     ins += 1
 
@@ -107,9 +159,55 @@ def stop():
 
 
 def posiciones():
-    a = person.xcor()
-    b = person.ycor()
-    return a, b
+    global Puntaje
+    a = yo.xcor()
+    b = yo.ycor()
+    dist1x = abs(a - cuadrado1.xcor())
+    dist1y = abs(b - cuadrado1.ycor())
+
+    dist2x = abs(a - rectangulo.xcor())
+    dist2y = abs(b - rectangulo.ycor())
+
+    dist3x = abs(a - circulosmall.xcor())
+    dist3y = abs(b - circulosmall.ycor())
+
+    dist4x = abs(a - circulo.xcor())
+    dist4y = abs(b - circulo.ycor())
+
+    dist5x = abs(a - circulobig.xcor())
+    dist5y = abs(b - circulobig.ycor())
+
+    dist6x = abs(a - triangulo.xcor())
+    dist6y = abs(b - triangulo.ycor())
+    if dist1x <= hitbox and dist1y <= hitbox:
+        retroced()
+        yo.direction = "stop"
+    if dist2x <= hitbox and dist2y <= hitbox:
+        retroced()
+        yo.direction = "stop"
+    if dist3x <= hitbox and dist3y <= hitbox and Puntaje == 0:
+        Puntaje = 1
+        circulosmall.hideturtle()
+        yo.shapesize(1.3, 1.3)
+        retroced()
+        yo.direction = "stop"
+
+    if dist4x <= hitbox and dist4y <= hitbox and Puntaje == 1:
+        Puntaje = 2
+        circulo.hideturtle()
+        yo.shapesize(1.5, 1.5)
+        retroced()
+        yo.direction = "stop"
+    if dist5x <= hitbox and dist5y <= hitbox and Puntaje == 2:
+        Puntaje = 3
+        circulobig.hideturtle()
+        yo.shapesize(1.7, 1.7)
+        retroced()
+        yo.direction = "stop"
+    if dist6x <= hitbox and dist6y <= hitbox:
+        yo.hideturtle()
+        yo.shapesize(1.7, 1.7)
+        yo.direction = "stop"
 
 
 # Configuracion lectura teclado
@@ -122,29 +220,10 @@ wn.onkeypress(stop, "Tab")
 
 
 # Principal Code
-ins = 0
-hitbox = 20
+
 while True:
     wn.update()
-
-    a, b = posiciones()
-    do1x = a - ogject1.xcor()
-    do1y = b - ogject1.ycor()
-
-    do2x = a - ogject2.xcor()
-    do2y = b - ogject2.ycor()
-
-    do1x = abs(do1x)
-    do1y = abs(do1y)
-    do2x = abs(do2x)
-    do2y = abs(do2y)
-    if do1x <= hitbox and do1y <= hitbox:
-        retroced()
-        person.direction = "stop"
-    if do2x <= hitbox and do2y <= hitbox:
-        retroced()
-        person.direction = "stop"
-
+    posiciones()
     mov()
     stop()
-    time.sleep(timestop)
+    time.sleep(aguanta)
